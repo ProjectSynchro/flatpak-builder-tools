c7610275c764c21ed482cecef319816af8a2d582
MarkS <marksvc@gmail.com>
2021-04-09 11:22:35 -0500

    dotnet: Handle multiple csproj files
    - Allows generator to create a single sources list from multiple
    .csproj files. A dependency may contain many .csproj files, but it
    makes sense to group the sources into one .json file for the
    dependency.
    e.g. flatpak-dotnet-generator output.json library1.csproj
    library2.csproj

--- dotnet/flatpak-dotnet-generator.py
+++ dotnet/flatpak-dotnet-generator.py
@@ -15,7 +15,7 @@ import tempfile
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('output', help='The output JSON sources file')
-    parser.add_argument('project', help='The project file')
+    parser.add_argument('project', nargs='+', help='The project file(s)')
     parser.add_argument('--runtime', '-r', help='The target runtime to restore packages for')
     parser.add_argument('--freedesktop', '-f', help='The target version of the freedesktop sdk to use', 
                         choices=['21.08', '22.08'], default='21.08')
@@ -33,14 +33,15 @@ def main():
         if args.runtime:
             runtime_args.extend(('-r', args.runtime))
 
-        subprocess.run([
-            'flatpak', 'run',
-            '--env=DOTNET_CLI_TELEMETRY_OPTOUT=true',
-            '--env=DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true',
-            '--command=sh', '--runtime=org.freedesktop.Sdk//{}'.format(args.freedesktop), '--share=network',
-            '--filesystem=host', 'org.freedesktop.Sdk.Extension.dotnet{}//{}'.format(args.dotnet, args.freedesktop), '-c',
-            'PATH="${PATH}:'+'/usr/lib/sdk/dotnet{0}/bin" LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/sdk/dotnet{0}/lib" exec dotnet restore "$@"'.format(args.dotnet),
-            '--', '--packages', tmp, args.project] + runtime_args)
+        for project in args.project:
+            subprocess.run([
+                'flatpak', 'run',
+                '--env=DOTNET_CLI_TELEMETRY_OPTOUT=true',
+                '--env=DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true',
+                '--command=sh', '--runtime=org.freedesktop.Sdk//{}'.format(args.freedesktop), '--share=network',
+                '--filesystem=host', 'org.freedesktop.Sdk.Extension.dotnet{}//{}'.format(args.dotnet, args.freedesktop), '-c',
+                'PATH="${PATH}:'+'/usr/lib/sdk/dotnet{0}/bin" LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/sdk/dotnet{0}/lib" exec dotnet restore "$@"'.format(args.dotnet),
+                '--', '--packages', tmp, args.project] + runtime_args)
 
         for path in Path(tmp).glob('**/*.nupkg.sha512'):
             name = path.parent.parent.name
